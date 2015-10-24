abec471737398063b0b2a0b1c4f0b472811d1483
Will Vega-Brown <wrvb@mit.edu>
2015-10-24 02:42:33 -0400

    Investigate requests

--- chatter/server.py
+++ chatter/server.py
@@ -5,11 +5,17 @@ import yaml
 import logging
 import flask
 
+import jenkins_bot
+
 config=yaml.load(file('/opt/chatter/config.yaml','r'))
 app = flask.Flask(__name__)
 logging.basicConfig(filename=config['log_file'], level=logging.INFO)
 
-@app.route('/chatter/')
-def index():
-    logging.info("Chattering!")
-    return "<span style='color red'>I am totally a flask!</span>"
+@app.route('/chatter/', methods['GET','POST'])
+def chatter():
+    logging.info(request)
+    if request.method == 'GET':
+        logging.info("Chattering! GET")
+    elif request.method == 'POST':
+        logging.info("Chattering! POST")
+    return "Success"
