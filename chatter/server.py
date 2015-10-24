ebc307618d03e53435072b366d509995ee9a7064
Will Vega-Brown <wrvb@mit.edu>
2015-10-24 12:57:45 -0400

    Remove duplicate file

--- chatter/server.py
+++ chatter/server.py
@@ -5,17 +5,24 @@ import yaml
 import logging
 import flask
 
-import jenkins_bot
+#import jenkins_bot
 
 config=yaml.load(file('/opt/chatter/config.yaml','r'))
 app = flask.Flask(__name__)
 logging.basicConfig(filename=config['log_file'], level=logging.INFO)
 
-@app.route('/chatter/', methods['GET','POST'])
+@app.route('/chatter/', methods=['GET','POST'])
 def chatter():
-    logging.info(request)
-    if request.method == 'GET':
+    if flask.request.method == 'GET':
         logging.info("Chattering! GET")
-    elif request.method == 'POST':
+        return "Success", 200
+    elif flask.request.method == 'POST':
         logging.info("Chattering! POST")
-    return "Success"
+        payload = flask.request.get_json()
+        event_type = flask.request.headers.get('X-Github-Event')
+	issue = payload['issue']['number']
+        logging.info("Received a comment on issue number {}".format(issue))
+	if 'pull_request' in payload['issue']:
+            logging.info("Issue is a pull request")
+	logging.info("Comment is {}".format(payload['comment']['body']))
+        return "Post acknowledged",204
